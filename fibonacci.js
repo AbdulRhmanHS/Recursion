3f5d1eae80466741435ba75a778ee68adde67ba3
AbdulRhman <bodahassan12@gmail.com>
2026-04-14 22:05:12 +0200

    Make the fibonacci function with both iterative and recursive solutions

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,34 @@
-function fibs() {
-  return 1;
+function fibs(n) {
+  let result = [];
+  if (n === 0) return result;
+  else {
+    for (let i = 0; i < n; i++) {
+      if (i >= 2) {
+        result.push(result[i - 1] + result[i - 2]);
+      } else if (i >= 1) {
+        result.push(1);
+      } else {
+        result.push(0);
+      }
+    }
+  }
+
+  return result;
 }
 
-module.exports = { fibs };
+function fibsRec(n) {
+  if (n === 0) return [];
+  if (n === 1) return [0];
+  if (n === 2) return [0, 1];
+
+  let previousState = fibsRec(n - 1);
+  let currentState = [0, 1];
+
+  for (let i = 0; i < previousState.length - 1; i++) {
+    currentState.push(previousState[i] + previousState[i + 1]);
+  }
+
+  return currentState;
+}
+
+module.exports = { fibs, fibsRec };
--- fibonacci.test.js
+++ fibonacci.test.js
@@ -1,5 +1,61 @@
-const { fibs } = require("./fibonacci");
+const { fibs, fibsRec } = require("./fibonacci");
 
-test("fibs returns 1", () => {
-  expect(fibs()).toBe(1);
+describe("fibs", () => {
+  test("returns empty array for 0", () => {
+    expect(fibs(0)).toEqual([]);
+  });
+
+  test("returns [0] for 1", () => {
+    expect(fibs(1)).toEqual([0]);
+  });
+
+  test("returns [0, 1] for 2", () => {
+    expect(fibs(2)).toEqual([0, 1]);
+  });
+
+  test("returns [0, 1, 1] for 3", () => {
+    expect(fibs(3)).toEqual([0, 1, 1]);
+  });
+
+  test("returns [0, 1, 1, 2] for 4", () => {
+    expect(fibs(4)).toEqual([0, 1, 1, 2]);
+  });
+
+  test("returns first 8 fibonacci numbers for 8", () => {
+    expect(fibs(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+  });
+
+  test("returns correct sequence for 10", () => {
+    expect(fibs(10)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+  });
+});
+
+describe("fibsRec", () => {
+  test("returns empty array for 0", () => {
+    expect(fibsRec(0)).toEqual([]);
+  });
+
+  test("returns [0] for 1", () => {
+    expect(fibsRec(1)).toEqual([0]);
+  });
+
+  test("returns [0, 1] for 2", () => {
+    expect(fibsRec(2)).toEqual([0, 1]);
+  });
+
+  test("returns [0, 1, 1] for 3", () => {
+    expect(fibsRec(3)).toEqual([0, 1, 1]);
+  });
+
+  test("returns [0, 1, 1, 2] for 4", () => {
+    expect(fibsRec(4)).toEqual([0, 1, 1, 2]);
+  });
+
+  test("returns first 8 fibonacci numbers for 8", () => {
+    expect(fibsRec(8)).toEqual([0, 1, 1, 2, 3, 5, 8, 13]);
+  });
+
+  test("returns correct sequence for 10", () => {
+    expect(fibsRec(10)).toEqual([0, 1, 1, 2, 3, 5, 8, 13, 21, 34]);
+  });
 });
